a4938e7e1d5aa61e46b12911c82cc773835d17ca
Isabel Hong <hong.isabel@gmail.com>
2021-02-24 15:25:41 -0300

    header conditional rendering

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,12 @@ import * as ROUTES from '../constants/routes';
 
 export default function Header() {
     const { firebase } = useContext(FirebaseContext);
+
+    const user = null;
+    // const user = {
+    //     displayName: "isabel"
+    // };
+
     return (
         <header className="h-16 bg-white border-b mb-8">
             <div className="container mx-auto max-width-lg h-full">
@@ -16,8 +22,52 @@ export default function Header() {
                             </Link>
                         </h1>
                     </div>
+                    <div className="text-gray text-center flex items-center align-items">
+                        {user ? (
+                            <>
+                                <Link to={ROUTES.DASHBOARD}>
+                                    <p>Dashboard</p>
+                                </Link>
+                                <button
+                                    type="button"
+                                    title="Sign Out"
+                                    onClick={() => firebase.auth().signOut()}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                            firebase.auth().signOut();
+                                        }
+                                    }}
+                                >
+                                    Sign Out
+                                </button>
+                                <div className="flex items-center cursor-pointer">
+                                    <Link to={`p/${user.displayName}`}>
+                                        <img
+                                            className="rounded-full h-8 w-8 flex"
+                                            src={`/images/avatars/${user.displayName}.jpg`}
+                                            alt={`${user.displayName} profile photo`}
+                                        />
+
+                                    </Link>
+                                </div>
+                            </>
+                        ) : (
+                                <>
+                                    <Link to={ROUTES.LOGIN}>
+                                        <button type="button" className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8">
+                                            Log In
+                                        </button>
+                                    </Link>
+                                    <Link to={ROUTES.SIGN_UP}>
+                                        <button type="button" className="font-bold text-sm rounded text-blue w-20 h-8">
+                                            Sign Up
+                                        </button>
+                                    </Link>
+                                </>
+                            )}
+                    </div>
                 </div>
             </div>
-        </header >
+        </header>
     );
-}
+};
