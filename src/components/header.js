b97ec8310c92818dddc7b7c40ea62bfcbfe99e5a
Isabel Hong <hong.isabel@gmail.com>
2021-02-24 15:35:56 -0300

    home and log out svg from heroicons added

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,10 @@ import * as ROUTES from '../constants/routes';
 export default function Header() {
     const { firebase } = useContext(FirebaseContext);
 
-    const user = null;
-    // const user = {
-    //     displayName: "isabel"
-    // };
+    // const user = null;
+    const user = {
+        displayName: "isabel"
+    };
 
     return (
         <header className="h-16 bg-white border-b mb-8">
@@ -26,7 +26,9 @@ export default function Header() {
                         {user ? (
                             <>
                                 <Link to={ROUTES.DASHBOARD}>
-                                    <p>Dashboard</p>
+                                    <svg className="w-8 mr-6 text-black-light cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                                    </svg>
                                 </Link>
                                 <button
                                     type="button"
@@ -38,7 +40,9 @@ export default function Header() {
                                         }
                                     }}
                                 >
-                                    Sign Out
+                                    <svg className="w-8 mr-6 text-black-light cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                                    </svg>
                                 </button>
                                 <div className="flex items-center cursor-pointer">
                                     <Link to={`p/${user.displayName}`}>
