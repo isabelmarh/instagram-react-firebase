9538da44f7dbe6e6df443e8fef46c7cc6fae28d4
Isabel Hong <hong.isabel@gmail.com>
2021-02-24 21:44:37 -0300

    useUser hook implemented in Sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import useUser from '../../hooks/use-user';
 
 export default function Sidebar() {
+    const { user: { docId, userId, following, username, fullName } = {} } = useUser();
+    console.log(user);
+
     return (
         <div>
             <p>Sidebar</p>
